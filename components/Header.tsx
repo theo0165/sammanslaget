64630acfc8c08d26522a80a9f12bee6188dad736
Theo Sandell <theo01sandell@gmail.com>
2022-08-23 09:45:23 +0200

    Create basic menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,32 @@
 import { NextPage } from "next";
+import Image from "next/image";
 import style from "../styles/Header.module.scss";
+import logo from "../public/logo.png";
+import { MdMenu } from "react-icons/md";
+import Link from "next/link";
 
 const Header: NextPage = () => {
   return (
     <header className={style.container}>
-      <div className={style.logo}></div>
-      <nav></nav>
+      <div className={style.logo}>
+        <Image src={logo} width={logo.width * 0.4} height={logo.height * 0.4} />
+      </div>
+      <nav>
+        <ul className={style.navList}>
+          <li>
+            <Link href="/">Hem</Link>
+          </li>
+          <li>
+            <Link href="/">Spela</Link>
+          </li>
+          <li>
+            <Link href="/">Om oss</Link>
+          </li>
+        </ul>
+        <div className={style.hamburger}>
+          <MdMenu size={"32px"} />
+        </div>
+      </nav>
     </header>
   );
 };
