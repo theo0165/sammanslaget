550051989d715c279bd403765d9c67fcd66092ef
Theo Sandell <theo01sandell@gmail.com>
2022-08-24 15:57:55 +0200

    New bof page

--- pages/bjorkafrihet.tsx
+++ pages/bjorkafrihet.tsx
@@ -6,20 +6,38 @@ import { MdArrowForwardIos } from "react-icons/md";
 const Bjorkafrihet: NextPage = () => {
   return (
     <>
-      <Header />
+      <Header noBg />
       <div className={style.container}>
-        <h1>Björk & Frihet</h1>
-        <p>
-          Mi tortor consequat ligula etiam senectus porttitor sollicitudin eget
-          habitant arcu cursus pretium hendrerit, laoreet turpis augue vulputate
-          parturient commodo fusce id montes mauris gravida. Senectus morbi
-          viverra sem ullamcorper duis, aenean porta pulvinar sit mattis
-          commodo, lacinia habitasse et felis. Est adipiscing suscipit efficitur
-          aliquet ipsum mi euismod elit urna class quam eleifend, praesent.
-        </p>
-        <div className={style.linkContainer}>
-          <a href="https://bjorkafrihet.se">Läs mer på Björk&frihets hemsida</a>
-          <MdArrowForwardIos />
+        <div>
+          <div className={style.topContent}>
+            <h1>Björk & Frihet</h1>
+            <p>
+              Björk & Frihet är en secondhandkedja som jobbar för ett hållbart
+              samhälle.
+              <br />
+              <br />
+              När du lämnar in en gåva på en av Björk&frihets insamlingsplatser
+              körs den till en av deras sorteringsanläggningar i Göteborg eller
+              Malmö. Där sorteras varje dag 4500 kg textilier och 5000 kg prylar
+              som säljs i någon av deras butiker. Pengarna som samlas in och
+              blir till överskott skänks till någon av deras
+              solidaritetsprojekt. I vårt spel sorterar du prylar och textilier
+              för att göra världen till en mer hållbar plats.
+            </p>
+            <div className={style.linkContainer}>
+              <a href="https://bjorkafrihet.se">
+                Läs mer på Björk&frihets hemsida
+              </a>
+              <MdArrowForwardIos />
+            </div>
+          </div>
+          <div className={style.bottomContent}>
+            <h2>Insamlingsplatser</h2>
+            <iframe
+              src="https://www.google.com/maps/d/embed?mid=13CKvW2JMO5WopARUYVF9TmZcxss&ehbc=2E312F&z=5"
+              className={style.map}
+            ></iframe>
+          </div>
         </div>
       </div>
     </>
