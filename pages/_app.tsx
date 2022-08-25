3ff3707a5bf473715e06301fed06522f0eb4be24
Theo Sandell <theo01sandell@gmail.com>
2022-08-25 13:14:58 +0200

    Add title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import Footer from "../components/Footer";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Sortera Mera!</title>
+      </Head>
       <Component {...pageProps} />
       <Footer />
     </>
